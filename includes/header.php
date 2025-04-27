a50c3ed7f9e0b00b33eb3addc18384d4080402e3
Dany fatihul <fatihuldanyy@gmail.com>
2025-04-27 20:52:16 +0700

    Add includes/header.php 1745761940205

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Aquanest - Depot Air Isi Ulang</title>
+    <link rel="stylesheet" href="/assets/css/style.css" />
+</head>
+<body>
