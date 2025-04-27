246bd26616147fe9cf768c7d312abd2e089d096d
Dany fatihul <fatihuldanyy@gmail.com>
2025-04-27 20:52:31 +0700

    Add user/about.php 1745761955005

---
+++ user/about.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require_once '../config/config.php';
+?>
+
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/navbar.php'; ?>
+
+<h2>Tentang Aquanest</h2>
+
+<p>Aquanest adalah depot air isi ulang yang menyediakan air berkualitas tinggi untuk kebutuhan sehari-hari Anda.</p>
+<p>Sejarah singkat depot air kami dimulai pada tahun 2010 dengan visi untuk menyediakan air bersih dan sehat bagi masyarakat sekitar.</p>
+<p>Visi kami adalah menjadi penyedia air isi ulang terpercaya dengan pelayanan terbaik.</p>
+<p>Misi kami meliputi:</p>
+<ul>
+    <li>Menyediakan air berkualitas dengan harga terjangkau.</li>
+    <li>Meningkatkan kepuasan pelanggan melalui pelayanan yang ramah dan cepat.</li>
+    <li>Menjaga kelestarian lingkungan dengan penggunaan kemasan ramah lingkungan.</li>
+</ul>
+
+<?php include '../includes/footer.php'; ?>
